b42c40c1a6f80bd9aee0e2bf6eb6b82c6d6c463d
Kryniol <pawel.krynicki@hotmail.com>
2015-04-08 11:43:03 +0200

    upgraded Google API client library to 1.0.*, adjusted the code

--- Security/Authentication/GoogleAuthenticator.php
+++ Security/Authentication/GoogleAuthenticator.php
@@ -2,28 +2,31 @@
 
 namespace Xsolve\GoogleAuthBundle\Security\Authentication;
 
-use Xsolve\GoogleAuthBundle\Security\Authentication\GoogleAuthenticationInterface;
+use FOS\UserBundle\Doctrine\UserManager;
+use FOS\UserBundle\Model\UserManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Xsolve\GoogleAuthBundle\Exception\NotAuthorizedException;
 use Xsolve\GoogleAuthBundle\Builder\ClientBuilderInterface;
 use Xsolve\GoogleAuthBundle\Builder\FOSUserBuilder;
-use Exception;
-use FOS\UserBundle\Model\UserManagerInterface;
-use Google_Oauth2Service;
+use Xsolve\GoogleAuthBundle\Exception\NotAuthorizedException;
+use Xsolve\GoogleAuthBundle\Security\Authentication\GoogleAuthenticationInterface;
 
 class GoogleAuthenticator implements GoogleAuthenticationInterface
 {
 
     /**
-     * @var \GoogleApi\Client
+     * @var \Google_Client
      */
     protected $client;
 
     /**
-     * @var \FOS\UserBundle\Doctrine\UserManager
+     * @var UserManager
      */
     protected $userManager;
 
+    /**
+     * @param ClientBuilderInterface $googleClientBuilder
+     * @param UserManagerInterface $userManager
+     */
     public function __construct(ClientBuilderInterface $googleClientBuilder, UserManagerInterface $userManager)
     {
         $this->client      = $googleClientBuilder->getClient();
@@ -31,38 +34,33 @@ class GoogleAuthenticator implements GoogleAuthenticationInterface
     }
 
     /**
-     * @return \FOS\UserBundle\Model\UserInterface
-     * @throws \Xsolve\GoogleAuthBundle\Exception\NotAuthorizedException
+     * {@inheritDoc}
      */
     public function authenticateUser(Request $request)
     {
-        if (!$request->get('code')) {
-
+        if (!$request->query->has('code')) {
             throw new NotAuthorizedException("There's missing authorization code");
         }
 
         $fosUserBuilder = new FOSUserBuilder($this->userManager);
-        $oauth2         = new Google_Oauth2Service($this->getClient());
+        $oauth2         = new \Google_Service_Oauth2($this->client);
 
         try {
-            $this->getClient()->authenticate();
-        } catch(Exception $e) {
-
-            throw new NotAuthorizedException(401, "XSolve Google Auth couldn't authorize user");
+            $this->client->authenticate($request->query->get('code'));
+        } catch(\Google_Auth_Exception $e) {
+            throw new NotAuthorizedException(401, "XSolve Google Auth couldn't authorize user", $e);
         }
 
         return $fosUserBuilder->build($oauth2->userinfo->get());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getAuthUrl()
     {
-        $url = $this->getClient()->createAuthUrl();
+        $url = $this->client->createAuthUrl();
 
         return preg_replace('/&approval_prompt=force/', '', $url);
-    }
-
-    protected function getClient()
-    {
-        return $this->client;
     }
 }
